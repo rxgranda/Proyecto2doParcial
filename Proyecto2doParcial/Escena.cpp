48af20b6bc23a281e1050e1d4d3b5733bb383fba
rxgranda <rxgranda@gmail.com>
2013-08-13 23:39:14 -0500

    Poner todas las lamparas

--- Proyecto2doParcial/Escena.cpp
+++ Proyecto2doParcial/Escena.cpp
@@ -35,13 +35,63 @@ GLfloat zPosicion = 13.0f;
 
 
 //luz
-GLfloat  whiteLight[] = { 0.2f, 0.2f, 0.2f, 1.0f };
-GLfloat  spot_direction[] = { 0.0f,-1.0f, 0.0f };
+//GLfloat  whiteLight[] = { 0.2f, 0.2f, 0.2f, 1.0f };
+
+GLfloat  spot0_direction[] = { 0.0f,-1.0f, 0.0f };
 GLfloat	 lightPos[] = { 0.0f, 8.0f, 0.0f,1.0f };
+GLfloat  light0x=-3.0f;
+GLfloat  light0z=-3.0f;
+
+//Segunda Luz
+GLfloat light1_diffuse[] = { 1.0, 1.0, 1.0, 1.0 };
+GLfloat light1_specular[] = { 1.0, 1.0, 1.0, 1.0 };
+GLfloat  spot1_direction[] = { 0.0f,1.0f, 0.0f };
+GLfloat  light1x=3.0f;
+GLfloat  light1z=-3.0f;
+
+GLfloat light2_diffuse[] = { 1.0, 1.0, 1.0, 1.0 };
+GLfloat light2_specular[] = { 1.0, 1.0, 1.0, 1.0 };
+GLfloat  spot2_direction[] = { 0.0f,-1.0f, 0.0f };
+GLfloat  light2x=-3.0f;
+GLfloat  light2z=0.0f;
+
+GLfloat light3_diffuse[] = { 1.0, 1.0, 1.0, 1.0 };
+GLfloat light3_specular[] = { 1.0, 1.0, 1.0, 1.0 };
+GLfloat  spot3_direction[] = { 0.0f,-1.0f, 0.0f };
+GLfloat  light3x=3.0f;
+GLfloat  light3z=0.0f;
+
+GLfloat light4_diffuse[] = { 1.0, 1.0, 1.0, 1.0 };
+GLfloat light4_specular[] = { 1.0, 1.0, 1.0, 1.0 };
+GLfloat  spot4_direction[] = { 0.0f,-1.0f, 0.0f };
+GLfloat  light4x=-3.0f;
+GLfloat  light4z=3.0f;
+
+GLfloat light5_diffuse[] = { 1.0, 1.0, 1.0, 1.0 };
+GLfloat light5_specular[] = { 1.0, 1.0, 1.0, 1.0 };
+GLfloat  spot5_direction[] = { 0.0f,-1.0f, 0.0f };
+GLfloat  light5x=3.0f;
+GLfloat  light5z=3.0f;
+
 
 // lista tablero
 GLuint _displayListId_AreaNegra; 
 GLuint _displayListId_AreaBlanca;
+
+
+
+//Cubo
+
+//Cantina
+
+//esfera
+
+
+//mastil
+
+
+//techo
+
 
 
 void dibujarEspacioNegro() {
@@ -203,6 +253,222 @@ void reshape(int w, int h){
 	glLoadIdentity();		
 }
 
+void luces(void){
+	
+	glPushMatrix();
+	glTranslatef(light0x,0.0f,light0z);
+		glLightf(GL_LIGHT0, GL_SPOT_CUTOFF, 25.0);
+		glLightfv(GL_LIGHT0, GL_SPOT_DIRECTION, spot0_direction);
+		glLightf(GL_LIGHT0, GL_SPOT_EXPONENT, 0.1);
+		glLightfv(GL_LIGHT0,GL_POSITION,lightPos);
+		
+	glPopMatrix();
+
+	glPushMatrix();
+		glTranslatef(light1x,0.0f,light1z);			
+
+		glLightfv(GL_LIGHT1, GL_DIFFUSE, light1_diffuse);
+		glLightfv(GL_LIGHT1, GL_SPECULAR, light1_specular);
+	
+	
+		glLightf(GL_LIGHT1, GL_CONSTANT_ATTENUATION, 1.0);
+		glLightf(GL_LIGHT1, GL_LINEAR_ATTENUATION, 0.0);
+		glLightf(GL_LIGHT1, GL_QUADRATIC_ATTENUATION, 0.0);
+
+		glLightf(GL_LIGHT1, GL_SPOT_CUTOFF, 25.0);
+		glLightfv(GL_LIGHT1, GL_SPOT_DIRECTION, spot0_direction);
+		glLightf(GL_LIGHT1, GL_SPOT_EXPONENT, 0.1);
+		glLightfv(GL_LIGHT1,GL_POSITION,lightPos);
+		
+	glPopMatrix();
+
+
+
+
+
+	glPushMatrix();
+		glTranslatef(light2x,0.0f,light2z);
+
+		glLightfv(GL_LIGHT2, GL_DIFFUSE, light2_diffuse);
+		glLightfv(GL_LIGHT2, GL_SPECULAR, light2_specular);
+	
+	
+		glLightf(GL_LIGHT2, GL_CONSTANT_ATTENUATION, 1.0);
+		glLightf(GL_LIGHT2, GL_LINEAR_ATTENUATION, 0.0);
+		glLightf(GL_LIGHT2, GL_QUADRATIC_ATTENUATION, 0.0);
+	
+		glLightf(GL_LIGHT2, GL_SPOT_CUTOFF, 25.0);
+		glLightfv(GL_LIGHT2, GL_SPOT_DIRECTION, spot2_direction);
+		glLightf(GL_LIGHT2, GL_SPOT_EXPONENT, 0.1);
+		glLightfv(GL_LIGHT2,GL_POSITION,lightPos);
+	
+	glPopMatrix();
+
+	glPushMatrix();
+
+
+		glTranslatef(light3x,0.0f,light3z);
+
+		glLightfv(GL_LIGHT3, GL_DIFFUSE, light3_diffuse);
+		glLightfv(GL_LIGHT3, GL_SPECULAR, light3_specular);
+	
+		glLightf(GL_LIGHT3, GL_CONSTANT_ATTENUATION, 1.0);
+		glLightf(GL_LIGHT3, GL_LINEAR_ATTENUATION, 0.0);
+		glLightf(GL_LIGHT3, GL_QUADRATIC_ATTENUATION, 0.0);
+
+		glLightf(GL_LIGHT3, GL_SPOT_CUTOFF, 25.0);
+		glLightfv(GL_LIGHT3, GL_SPOT_DIRECTION, spot3_direction);
+		glLightf(GL_LIGHT3, GL_SPOT_EXPONENT, 0.1);
+		glLightfv(GL_LIGHT3,GL_POSITION,lightPos);
+
+	glPopMatrix();
+
+	glPushMatrix();
+	glTranslatef(light4x,0.0f,light4z);
+
+	glLightfv(GL_LIGHT4, GL_DIFFUSE, light4_diffuse);
+		glLightfv(GL_LIGHT4, GL_SPECULAR, light4_specular);
+	
+		glLightf(GL_LIGHT4, GL_CONSTANT_ATTENUATION, 1.0);
+		glLightf(GL_LIGHT4, GL_LINEAR_ATTENUATION, 0.0);
+		glLightf(GL_LIGHT4, GL_QUADRATIC_ATTENUATION, 0.0);
+
+		glLightf(GL_LIGHT4, GL_SPOT_CUTOFF, 25.0);
+		glLightfv(GL_LIGHT4, GL_SPOT_DIRECTION, spot4_direction);
+		glLightf(GL_LIGHT4, GL_SPOT_EXPONENT, 0.1);
+		glLightfv(GL_LIGHT4,GL_POSITION,lightPos);
+		
+	glPopMatrix();
+
+	glPushMatrix();
+	glTranslatef(light5x,0.0f,light5z);
+
+	glLightfv(GL_LIGHT5, GL_DIFFUSE, light5_diffuse);
+		glLightfv(GL_LIGHT5, GL_SPECULAR, light5_specular);
+	
+		glLightf(GL_LIGHT5, GL_CONSTANT_ATTENUATION, 1.0);
+		glLightf(GL_LIGHT5, GL_LINEAR_ATTENUATION, 0.0);
+		glLightf(GL_LIGHT5, GL_QUADRATIC_ATTENUATION, 0.0);
+
+		glLightf(GL_LIGHT5, GL_SPOT_CUTOFF, 25.0);
+		glLightfv(GL_LIGHT5, GL_SPOT_DIRECTION, spot5_direction);
+		glLightf(GL_LIGHT5, GL_SPOT_EXPONENT, 0.1);
+		glLightfv(GL_LIGHT5,GL_POSITION,lightPos);
+		
+	glPopMatrix();
+
+
+	glEnable(GL_LIGHT0);
+	glEnable(GL_LIGHT1);
+	glEnable(GL_LIGHT2);
+	glEnable(GL_LIGHT3);
+	glEnable(GL_LIGHT4);
+	glEnable(GL_LIGHT5);
+	
+}
+
+void lamparas(void){
+	GLfloat no_emitir[] = { 0.0, 0.0, 0.0, 1.0 };
+
+	//lampara1
+	glPushMatrix();	
+		glTranslatef(light0x,7.9f,light0z);
+		glColor3f(1.0f,0.0f,0.0f);
+		GLfloat lampara1[] = {1.0, 1.0, 1.0};
+	
+		 glMaterialfv(GL_FRONT_AND_BACK, GL_EMISSION,lampara1);
+		glBegin(GL_QUADS);
+		
+			glVertex3f(-1.0f,0.0f,-1.0f);
+			glVertex3f(-1.0f,0.0f,1.0f);
+			glVertex3f(1.0f,0.0f,1.0f);
+			glVertex3f(1.0f,0.0f,-1.0f);											
+		glEnd();
+
+	glPopMatrix();
+	//lampara2
+	glPushMatrix();	
+		glTranslatef(light1x,7.9f,light1z);
+		glColor3f(1.0f,0.0f,0.0f);
+		GLfloat lampara2[] = {1.0, 1.0, 1.0};
+
+		 glMaterialfv(GL_FRONT_AND_BACK, GL_EMISSION,lampara2);
+		glBegin(GL_QUADS);
+		
+			glVertex3f(-1.0f,0.0f,-1.0f);
+			glVertex3f(-1.0f,0.0f,1.0f);
+			glVertex3f(1.0f,0.0f,1.0f);
+			glVertex3f(1.0f,0.0f,-1.0f);											
+		glEnd();
+
+	glPopMatrix();
+	//lampara3
+	glPushMatrix();	
+		glTranslatef(light2x,7.9f,light2z);
+		glColor3f(1.0f,0.0f,0.0f);
+		GLfloat lampara3[] = {1.0, 1.0, 1.0};
+	
+		 glMaterialfv(GL_FRONT_AND_BACK, GL_EMISSION,lampara3);
+		glBegin(GL_QUADS);
+		
+			glVertex3f(-1.0f,0.0f,-1.0f);
+			glVertex3f(-1.0f,0.0f,1.0f);
+			glVertex3f(1.0f,0.0f,1.0f);
+			glVertex3f(1.0f,0.0f,-1.0f);											
+		glEnd();
+
+	glPopMatrix();
+	//lampara4
+	glPushMatrix();	
+		glTranslatef(light3x,7.9f,light3z);
+		glColor3f(1.0f,0.0f,0.0f);
+		GLfloat lampara4[] = {1.0, 1.0, 1.0};
+
+		 glMaterialfv(GL_FRONT_AND_BACK, GL_EMISSION,lampara4);
+		glBegin(GL_QUADS);
+		
+			glVertex3f(-1.0f,0.0f,-1.0f);
+			glVertex3f(-1.0f,0.0f,1.0f);
+			glVertex3f(1.0f,0.0f,1.0f);
+			glVertex3f(1.0f,0.0f,-1.0f);											
+		glEnd();
+
+	glPopMatrix();
+	//lampara5
+	glPushMatrix();	
+		glTranslatef(light4x,7.9f,light4z);
+		glColor3f(1.0f,0.0f,0.0f);
+		GLfloat lampara5[] = {1.0, 1.0, 1.0};
+
+		 glMaterialfv(GL_FRONT_AND_BACK, GL_EMISSION,lampara5);
+		glBegin(GL_QUADS);
+		
+			glVertex3f(-1.0f,0.0f,-1.0f);
+			glVertex3f(-1.0f,0.0f,1.0f);
+			glVertex3f(1.0f,0.0f,1.0f);
+			glVertex3f(1.0f,0.0f,-1.0f);											
+		glEnd();
+
+	glPopMatrix();
+	//lampara6
+	glPushMatrix();	
+		glTranslatef(light5x,7.9f,light5z);
+		glColor3f(1.0f,0.0f,0.0f);
+		GLfloat lampara6[] = {1.0, 1.0, 1.0};		
+		 glMaterialfv(GL_FRONT_AND_BACK, GL_EMISSION,lampara6);
+		glBegin(GL_QUADS);
+		
+			glVertex3f(-1.0f,0.0f,-1.0f);
+			glVertex3f(-1.0f,0.0f,1.0f);
+			glVertex3f(1.0f,0.0f,1.0f);
+			glVertex3f(1.0f,0.0f,-1.0f);											
+		glEnd();
+		
+	glPopMatrix();
+	//Deshabilitar emision
+	glMaterialfv(GL_FRONT_AND_BACK, GL_EMISSION,no_emitir);
+}
+
 
 /*
 * Procedimiento: display
@@ -226,15 +492,7 @@ void display (void){
 	
 	glTranslatef(xPosicion,yPosicion,0);
 
-	glPushMatrix();
-	//glTranslatef(5.0f,0.0f,5.0f);
-	glLightf(GL_LIGHT0, GL_SPOT_CUTOFF, 15.0);
-	glLightfv(GL_LIGHT0, GL_SPOT_DIRECTION, spot_direction);
-	glLightf(GL_LIGHT0, GL_SPOT_EXPONENT, 0.1);
-	glLightfv(GL_LIGHT0,GL_POSITION,lightPos);
-	glEnable(GL_LIGHT0);
-
-	glPopMatrix();
+	luces();
 
 	glPushMatrix();	
 	
@@ -311,6 +569,10 @@ void display (void){
 
 	//techo
 
+	
+
+
+
 	glPushMatrix();	
 	glTranslatef(-6.0f,8.0f,6.0f);
 	glBegin(GL_QUADS);
@@ -320,10 +582,15 @@ void display (void){
 	glVertex3f(12.0f,0.0f,-12.0f);
 	glVertex3f(0.0f,0.0f,-12.0f);
 	glEnd();
-
-	
-	
 	glPopMatrix();
+	
+	
+	
+	
+	//lamparas
+	lamparas();
+	
+	
 
 	
 	
@@ -345,13 +612,14 @@ void init (void){
 
 	// habilitar luz
 	glEnable(GL_LIGHTING);
+	 glEnable(GL_NORMALIZE);
 
 	// configurar y habilitar luz
 	//glLightModelfv(GL_LIGHT_MODEL_AMBIENT,whiteLight);
 	//glLightfv(GL_LIGHT0,GL_DIFFUSE,sourceLight);
-	glLightfv(GL_LIGHT0,GL_POSITION,lightPos);
-	glEnable(GL_LIGHT0);
-
+	/*glLightfv(GL_LIGHT1,GL_POSITION,lightPos);
+	glEnable(GL_LIGHT1);
+*/
 	// Habilitar el rastreo de color
 	glEnable(GL_COLOR_MATERIAL);
 
